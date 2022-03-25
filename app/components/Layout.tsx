debcd1be3f131bb54bf15dca73948f800ae11b17
Rickard Natt och Dag <rdag@invidi.com>
2022-03-25 09:17:59 +0100

    chore: fix type errors

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,6 +1,6 @@
 import { NavLink } from "remix";
 
-export default function Layout({ children }: { children: JSX.Element }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="grid-template-main mt-8 grid md:px-8">
       <div className="col-start-3 col-end-4 flex flex-col space-y-2 border-b border-gray-200 pb-8 md:flex-row md:items-center md:justify-end md:space-y-0 md:space-x-8 md:border-0 md:pb-0">
