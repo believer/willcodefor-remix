1c98a9bef96ad87d6081dd0a740a22023b9f3d2a
Rickard Natt och Dag <rdag@invidi.com>
2022-03-25 09:48:22 +0100

    fix: posts page title and list styling

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -16,33 +16,37 @@ export default function PostsIndexPage() {
   const data = useLoaderData() as LoaderData;
 
   return (
-    <ol
-      reversed
-      className="space-y-2 sm:space-y-4"
-      style={{ counterReset: `section ${data.posts.length + 1}` }}
-    >
-      {data.posts.map((post) => (
-        <li
-          className="grid-post counter-decrement relative grid items-baseline gap-4 sm:gap-5"
-          key={post.id}
-        >
-          <Link to={`/posts/${post.slug}`} prefetch="intent">
-            {post.title}
-          </Link>
-          <hr className="m-0 hidden border-dashed border-gray-300 sm:block" />
-          <time
-            className="font-mono text-xs tabular-nums text-gray-500"
-            dateTime={toISO(post.createdAt)}
+    <div className="mx-auto max-w-2xl">
+      <h2>Today I Learned</h2>
+
+      <ol
+        reversed
+        className="space-y-2 sm:space-y-4"
+        style={{ counterReset: `section ${data.posts.length + 1}` }}
+      >
+        {data.posts.map((post) => (
+          <li
+            className="grid-post counter-decrement relative grid items-baseline gap-4 sm:gap-5"
+            key={post.id}
           >
-            <span className="hidden sm:block">
-              {formatDateTime(post.createdAt)}
-            </span>
-            <span className="block sm:hidden">
-              {formatDate(post.createdAt)}
-            </span>
-          </time>
-        </li>
-      ))}
-    </ol>
+            <Link to={`/posts/${post.slug}`} prefetch="intent">
+              {post.title}
+            </Link>
+            <hr className="m-0 hidden border-dashed border-gray-300 sm:block" />
+            <time
+              className="font-mono text-xs tabular-nums text-gray-500"
+              dateTime={toISO(post.createdAt)}
+            >
+              <span className="hidden sm:block">
+                {formatDateTime(post.createdAt)}
+              </span>
+              <span className="block sm:hidden">
+                {formatDate(post.createdAt)}
+              </span>
+            </time>
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 }
