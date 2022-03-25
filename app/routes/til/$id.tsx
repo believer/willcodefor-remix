4e1088e97e503754666b7c126c594e72e2fefdf0
Rickard Natt och Dag <rickard@nattochdag.dev>
2022-03-25 22:00:04 +0100

    chore: cleanup til redirect

--- app/routes/til/$id.tsx
+++ app/routes/til/$id.tsx
@@ -1,4 +1,4 @@
-import { Link, LoaderFunction, redirect, useCatch } from 'remix'
+import { LoaderFunction, redirect } from 'remix'
 import { prisma } from '~/db.server'
 
 export const loader: LoaderFunction = async ({ params }) => {
@@ -16,27 +16,4 @@ export const loader: LoaderFunction = async ({ params }) => {
   }
 
   return redirect(`/posts/${post.slug}`)
-}
-
-export function ErrorBoundary({ error }: { error: Error }) {
-  console.error(error)
-
-  return <div>An unexpected error occurred: {error.message}</div>
-}
-
-export function CatchBoundary() {
-  const caught = useCatch()
-
-  console.log(caught)
-
-  if (caught.status === 404) {
-    return (
-      <div>
-        Post not found. Try another one from the{' '}
-        <Link to="/posts">posts list</Link>
-      </div>
-    )
-  }
-
-  throw new Error(`Unexpected caught response with status: ${caught.status}`)
 }
