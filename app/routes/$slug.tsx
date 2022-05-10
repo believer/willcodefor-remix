7eb9d14d00ef6954b1faa2ca6808e252d25ce2a6
Rickard Natt och Dag <rdag@invidi.com>
2022-05-10 13:53:14 +0200

    feat: add ability to link to post from root domain

---
+++ app/routes/$slug.tsx
@@ -0,0 +1,19 @@
+import { LoaderFunction, redirect } from 'remix'
+import { prisma } from '~/db.server'
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const post = await prisma.post.findFirst({
+    where: {
+      OR: [{ longSlug: params.slug }, { slug: params.slug }],
+    },
+    select: {
+      slug: true,
+    },
+  })
+
+  if (!post) {
+    return redirect(`/posts/404`)
+  }
+
+  return redirect(`/posts/${post.slug}`)
+}
