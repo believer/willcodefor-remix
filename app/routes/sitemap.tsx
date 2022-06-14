fe20a577151fd4c70506b77a06dfbec71f2d48c1
Rickard Natt och Dag <rickard@nattochdag.dev>
2022-06-14 13:15:40 +0200

    feat: add sitemap

---
+++ app/routes/sitemap.tsx
@@ -0,0 +1,36 @@
+import type { LoaderFunction } from 'remix'
+import { getPosts } from '~/models/post.server'
+import { toISO } from '~/utils/intl'
+
+export const loader: LoaderFunction = async () => {
+  const posts = await getPosts()
+  const metadata = {
+    title: 'willcodefor.beer',
+    url: 'https://willcodefor.beer/',
+    description: 'Things I learn while browsing the web',
+    author: {
+      name: 'Rickard Natt och Dag',
+      email: 'rickard@willcodefor.dev',
+    },
+  }
+
+  const sitemap = `<?xml version="1.0" encoding="utf-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${posts
+      .map(
+        (post) =>
+          `<url>
+      <loc>${metadata.url}${post.slug}</loc>
+      <lastmod>${toISO(post.updatedAt)}</lastmod>
+    </url>`
+      )
+      .join('')}
+</urlset>`
+
+  return new Response(sitemap, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  })
+}
