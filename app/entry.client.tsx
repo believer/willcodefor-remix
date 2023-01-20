c8c151e23ab0df7620f192074cf9a63c9b28fdb4
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-01-20 15:34:33 +0100

    chore: maybe this solves hydration
    probably not though

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,14 +1,22 @@
-import * as React from 'react'
 import { RemixBrowser } from '@remix-run/react'
+import { startTransition, StrictMode } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 
-setTimeout(() => {
-  React.startTransition(() => {
+function hydrate() {
+  startTransition(() => {
     hydrateRoot(
       document,
-      <React.StrictMode>
+      <StrictMode>
         <RemixBrowser />
-      </React.StrictMode>
+      </StrictMode>
     )
   })
-}, 10)
+}
+
+if (typeof requestIdleCallback === 'function') {
+  requestIdleCallback(hydrate)
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  setTimeout(hydrate, 1)
+}
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,23 +1,53 @@
-import type { HandleDocumentRequestFunction } from '@remix-run/node'
+import { PassThrough } from 'stream'
+import type { EntryContext } from '@remix-run/node'
+import { Response } from '@remix-run/node'
 import { RemixServer } from '@remix-run/react'
-import { renderToString } from 'react-dom/server'
+import isbot from 'isbot'
+import { renderToPipeableStream } from 'react-dom/server'
 
-let handleDocumentRequest: HandleDocumentRequestFunction = async (
-  request,
-  responseStatusCode,
-  responseHeaders,
-  remixContext
-) => {
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
-  )
+const ABORT_DELAY = 5000
 
-  responseHeaders.set('Content-Type', 'text/html')
+export default function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
+) {
+  const callbackName = isbot(request.headers.get('user-agent'))
+    ? 'onAllReady'
+    : 'onShellReady'
 
-  return new Response('<!DOCTYPE html>' + markup, {
-    status: responseStatusCode,
-    headers: responseHeaders,
+  return new Promise((resolve, reject) => {
+    let didError = false
+
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer context={remixContext} url={request.url} />,
+      {
+        [callbackName]: () => {
+          const body = new PassThrough()
+
+          responseHeaders.set('Content-Type', 'text/html')
+
+          resolve(
+            new Response(body, {
+              headers: responseHeaders,
+              status: didError ? 500 : responseStatusCode,
+            })
+          )
+
+          pipe(body)
+        },
+        onShellError: (err: unknown) => {
+          reject(err)
+        },
+        onError: (error: unknown) => {
+          didError = true
+
+          console.error(error)
+        },
+      }
+    )
+
+    setTimeout(abort, ABORT_DELAY)
   })
 }
-
-export default handleDocumentRequest
