97e7b1e70c366f26b526f131cdc66bd8b4986a75
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-02-18 11:03:58 +0100

    fix(habits): duration has initial value

--- app/routes/habits/HumanDuration.tsx
+++ app/routes/habits/HumanDuration.tsx
@@ -2,24 +2,32 @@ import humanizeDuration from 'humanize-duration'
 import { DateTime } from 'luxon'
 import React from 'react'
 
-export default function HumanDuration({ startDate }: { startDate: DateTime }) {
-  const [time, setTime] = React.useState<string>('')
+const useTick = (startDate: DateTime) => {
+  const difference = React.useCallback(() => {
+    const now = DateTime.now()
+    const diff = now.diff(startDate, 'seconds').toMillis()
+
+    return humanizeDuration(diff, { round: true })
+  }, [startDate])
+
+  const [time, setTime] = React.useState<string>(difference)
 
   React.useEffect(() => {
-    const interval = setInterval(() => {
-      const now = DateTime.now()
-      const diff = now.diff(startDate, 'seconds').toMillis()
-
-      setTime(() => humanizeDuration(diff, { round: true }))
-    }, 1000)
+    const interval = setInterval(() => setTime(difference()), 1000)
 
     return () => {
       clearInterval(interval)
     }
-  }, [startDate])
+  }, [difference])
+
+  return time
+}
+
+export default function HumanDuration({ startDate }: { startDate: DateTime }) {
+  const time = useTick(startDate)
 
   return (
-    <div className="mt-2 space-y-1 text-center text-xs text-slate-500">
+    <div className="mt-2 space-y-1 text-center text-xs text-slate-600">
       {time}
     </div>
   )
