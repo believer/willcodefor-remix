c1a5ebca5c4c7d3ad204cd27afc019baa1229ab8
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-02-18 11:03:46 +0100

    feat(habits): add manifest file

--- app/routes/habits/route.tsx
+++ app/routes/habits/route.tsx
@@ -1,10 +1,16 @@
+import type { LinksFunction } from '@remix-run/node'
 import clsx from 'clsx'
 import { DateTime } from 'luxon'
 import HumanDuration from './HumanDuration'
+import habitsManifest from './manifest.webmanifest'
 
 type Habit = {
   startDate: DateTime | null
   title: string
+}
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'manifest', href: habitsManifest }]
 }
 
 export default function HabitsPage() {
@@ -17,6 +23,10 @@ export default function HabitsPage() {
       title: 'No caffeine',
       startDate: null,
     },
+    {
+      title: 'No junk food',
+      startDate: null,
+    },
   ]
 
   const now = DateTime.now()
@@ -24,42 +34,47 @@ export default function HabitsPage() {
   const colors = [
     'bg-gradient-to-br from-emerald-500 to-emerald-700 ring-emerald-500/30',
     'bg-gradient-to-br from-cyan-500 to-cyan-700 ring-cyan-500/30',
+    'bg-gradient-to-br from-rose-500 to-rose-700 ring-rose-500/30',
   ]
 
   return (
     <div className="grid grid-cols-1 gap-x-8 gap-y-4 md:grid-cols-2">
-      {habits.map((habit, index) => {
-        const days = now.diff(habit.startDate ?? now, 'days').toFormat('d')
+      {habits
+        .filter(({ startDate }) => startDate)
+        .map((habit, index) => {
+          const days = now.diff(habit.startDate ?? now, 'days').toFormat('d')
 
-        return (
-          <div className="group" key={habit.title}>
-            <div
-              className={clsx([
-                'flex items-end justify-between rounded-lg px-5 pt-16 pb-4 ring-offset-4 ring-offset-tokyoNight-bg hover:ring-2',
-                colors[index],
-              ])}
-            >
-              <time
-                dateTime={habit.startDate?.toISODate()}
-                title={habit.startDate?.toFormat('cccc, LLLL d, yyyy HH:mm')}
+          return (
+            <div className="group" key={habit.title}>
+              <div
+                className={clsx([
+                  'flex items-end justify-between rounded-lg px-5 pt-16 pb-4 ring-offset-4 ring-offset-tokyoNight-bg hover:ring-2',
+                  colors[index],
+                ])}
               >
-                <div className="text-2xl font-bold text-slate-800">{days}</div>
-                <div className="text-xs text-slate-800">days</div>
-              </time>
-              <div className="text-sm text-emerald-900">{habit.title}</div>
+                <time
+                  dateTime={habit.startDate?.toISODate()}
+                  title={habit.startDate?.toFormat('cccc, LLLL d, yyyy HH:mm')}
+                >
+                  <div className="text-2xl font-bold text-slate-800">
+                    {days}
+                  </div>
+                  <div className="text-xs text-slate-800">days</div>
+                </time>
+                <div className="text-sm text-emerald-900">{habit.title}</div>
+              </div>
+              <div className="">
+                {habit.startDate ? (
+                  <HumanDuration startDate={habit.startDate} />
+                ) : (
+                  <div className="mt-2 text-center text-xs text-slate-500">
+                    Not started
+                  </div>
+                )}
+              </div>
             </div>
-            <div className="invisible group-hover:visible">
-              {habit.startDate ? (
-                <HumanDuration startDate={habit.startDate} />
-              ) : (
-                <div className="mt-2 text-center text-xs text-slate-500">
-                  Not started
-                </div>
-              )}
-            </div>
-          </div>
-        )
-      })}
+          )
+        })}
     </div>
   )
 }
