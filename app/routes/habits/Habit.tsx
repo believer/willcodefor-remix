a75dea729b76a59f74fde1e62b12cd433c950ccc
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-02-19 18:39:45 +0100

    feat(habits): update design

---
+++ app/routes/habits/Habit.tsx
@@ -0,0 +1,99 @@
+import clsx from 'clsx'
+import type { DurationObjectUnits } from 'luxon'
+import { DateTime } from 'luxon'
+import React from 'react'
+
+export type Habit = {
+  color: string
+  startDate: DateTime
+  title: string
+}
+
+type HabitProps = {
+  habit: Habit
+}
+
+type NoOptionals<T> = {
+  [K in keyof T]-?: T[K]
+}
+
+type Difference = NoOptionals<
+  Pick<
+    DurationObjectUnits,
+    'years' | 'months' | 'weeks' | 'days' | 'hours' | 'minutes' | 'seconds'
+  >
+>
+
+const useTick = (startDate: Habit['startDate']) => {
+  const difference = React.useCallback((): Difference => {
+    const now = DateTime.now()
+
+    return now
+      .diff(startDate ?? now, [
+        'years',
+        'months',
+        'weeks',
+        'days',
+        'hours',
+        'minutes',
+        'seconds',
+      ])
+      .toObject() as Difference
+  }, [startDate])
+
+  const [time, setTime] = React.useState<Difference>(difference)
+
+  React.useEffect(() => {
+    const interval = setInterval(() => setTime(difference()), 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [difference])
+
+  return time
+}
+
+export default function HabitView({ habit }: HabitProps) {
+  const parts = useTick(habit.startDate)
+  const now = DateTime.now()
+  const days = now.diff(habit.startDate ?? now, 'days').toFormat('d')
+  const [displayBack, setDisplayBack] = React.useState(false)
+
+  return (
+    <button
+      className={clsx([
+        'group flex h-32 items-end justify-between rounded-lg px-5 pb-4 text-left ring-offset-4 ring-offset-tokyoNight-bg hover:ring-2',
+        habit.color,
+      ])}
+      key={habit.title}
+      onClick={() => setDisplayBack(!displayBack)}
+    >
+      {displayBack ? (
+        <div className="grid grid-cols-7 gap-4">
+          {Object.entries(parts)
+            .filter(([_, value]) => value !== 0)
+            .map(([key, value]) => (
+              <div className="text-2xl font-bold text-slate-800" key={key}>
+                {Math.floor(value)}
+                <div className="text-xs font-normal">
+                  {Math.floor(value) === 1 ? key.slice(0, -1) : key}
+                </div>
+              </div>
+            ))}
+        </div>
+      ) : (
+        <>
+          <time
+            dateTime={habit.startDate.toISODate()}
+            title={habit.startDate.toFormat('cccc, LLLL d, yyyy HH:mm')}
+          >
+            <div className="text-2xl font-bold text-slate-800">{days}</div>
+            <div className="text-xs text-slate-800">days</div>
+          </time>
+          <div className="text-sm text-emerald-900">{habit.title}</div>
+        </>
+      )}
+    </button>
+  )
+}
--- app/routes/habits/route.tsx
+++ app/routes/habits/route.tsx
@@ -1,80 +1,33 @@
 import type { LinksFunction } from '@remix-run/node'
-import clsx from 'clsx'
 import { DateTime } from 'luxon'
-import HumanDuration from './HumanDuration'
+import type { Habit } from './Habit'
+import HabitView from './Habit'
 import habitsManifest from './manifest.webmanifest'
-
-type Habit = {
-  startDate: DateTime | null
-  title: string
-}
 
 export const links: LinksFunction = () => {
   return [{ rel: 'manifest', href: habitsManifest }]
 }
 
+const habits: Habit[] = [
+  {
+    title: 'No alcohol',
+    startDate: DateTime.fromISO('2023-01-29T20:30:32.000Z'),
+    color:
+      'bg-gradient-to-br from-emerald-500 to-emerald-700 ring-emerald-500/30',
+  },
+  {
+    title: 'No coffee',
+    startDate: DateTime.fromISO('2023-02-18T14:35:14.000Z'),
+    color: 'bg-gradient-to-br from-cyan-500 to-cyan-700 ring-cyan-500/30',
+  },
+]
+
 export default function HabitsPage() {
-  const habits: Habit[] = [
-    {
-      title: 'No alcohol',
-      startDate: DateTime.fromISO('2023-01-29T20:30:32.000Z'),
-    },
-    {
-      title: 'No coffee',
-      startDate: DateTime.fromISO('2023-02-18T14:35:14.000Z'),
-    },
-    {
-      title: 'No junk food',
-      startDate: null,
-    },
-  ]
-
-  const now = DateTime.now()
-
-  const colors = [
-    'bg-gradient-to-br from-emerald-500 to-emerald-700 ring-emerald-500/30',
-    'bg-gradient-to-br from-cyan-500 to-cyan-700 ring-cyan-500/30',
-    'bg-gradient-to-br from-rose-500 to-rose-700 ring-rose-500/30',
-  ]
-
   return (
-    <div className="grid grid-cols-1 gap-x-8 gap-y-4 md:grid-cols-2">
-      {habits
-        .filter(({ startDate }) => startDate)
-        .map((habit, index) => {
-          const days = now.diff(habit.startDate ?? now, 'days').toFormat('d')
-
-          return (
-            <div className="group" key={habit.title}>
-              <div
-                className={clsx([
-                  'flex items-end justify-between rounded-lg px-5 pt-16 pb-4 ring-offset-4 ring-offset-tokyoNight-bg hover:ring-2',
-                  colors[index],
-                ])}
-              >
-                <time
-                  dateTime={habit.startDate?.toISODate()}
-                  title={habit.startDate?.toFormat('cccc, LLLL d, yyyy HH:mm')}
-                >
-                  <div className="text-2xl font-bold text-slate-800">
-                    {days}
-                  </div>
-                  <div className="text-xs text-slate-800">days</div>
-                </time>
-                <div className="text-sm text-emerald-900">{habit.title}</div>
-              </div>
-              <div className="">
-                {habit.startDate ? (
-                  <HumanDuration startDate={habit.startDate} />
-                ) : (
-                  <div className="mt-2 text-center text-xs text-slate-500">
-                    Not started
-                  </div>
-                )}
-              </div>
-            </div>
-          )
-        })}
+    <div className="grid grid-cols-1 gap-x-8 gap-y-6 md:grid-cols-2">
+      {habits.map((habit) => (
+        <HabitView key={habit.title} habit={habit} />
+      ))}
     </div>
   )
 }
