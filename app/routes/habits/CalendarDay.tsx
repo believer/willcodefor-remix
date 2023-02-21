bfaa575aa47a97c08b3b1f8780a519b03a90c909
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-02-21 15:32:52 +0100

    feat(habits): display progress from start time, change end to next day

--- app/routes/habits/CalendarDay.tsx
+++ app/routes/habits/CalendarDay.tsx
@@ -50,24 +50,40 @@ export default function CalendarDay({ day, month, habit }: CalendarDayProps) {
     currentDay.get('day') === now.get('day') &&
     currentDay.get('month') === now.get('month') &&
     currentDay.get('year') === now.get('year')
+  const isStartDay =
+    currentDay.get('day') === startDate.get('day') &&
+    currentDay.get('month') === startDate.get('month') &&
+    currentDay.get('year') === startDate.get('year')
 
-  if (isCurrentDay && !hasOccuredToday()) {
-    const hoursUntilNext = () =>
-      now
-        .set({
-          hour: startDate.get('hour'),
-          minute: startDate.get('minute'),
-          second: startDate.get('second'),
-        })
-        .diff(now, ['hours'])
-        .toObject().hours ?? 0
-    const percentComplete = () => ((24 - hoursUntilNext()) / 24) * 100
+  // Display a progress bar from the start date/time to the end of the day
+  if (isStartDay) {
+    const hoursUntilEndOfDay =
+      startDate.endOf('day').diff(startDate, ['hours']).toObject().hours ?? 0
+    // Since we are going backwards, we need to subtract by 100 to get
+    // the remaining percentage of the day
+    const percentComplete = 100 - ((24 - hoursUntilEndOfDay) / 24) * 100
 
     return (
-      <div className="relative mt-1 h-1 bg-slate-800">
+      <div className="relative mt-1 h-1 rounded-tl-lg rounded-bl-lg bg-slate-800">
         <div
-          className={`absolute top-0 left-0 h-1 ${calendarColor}`}
-          style={{ width: `${percentComplete()}%` }}
+          className={`absolute top-0 right-0 h-1 ${calendarColor} rounded-tl-lg rounded-bl-lg`}
+          style={{ width: `${percentComplete}%` }}
+        />
+      </div>
+    )
+  }
+
+  // Display a progress bar from the start of the day to the current time
+  if (isCurrentDay && !hasOccuredToday()) {
+    const hoursUntilNext =
+      now.endOf('day').diff(now, ['hours']).toObject().hours ?? 0
+    const percentComplete = ((24 - hoursUntilNext) / 24) * 100
+
+    return (
+      <div className="relative mt-1 h-1 rounded-tr-lg rounded-br-lg bg-slate-800">
+        <div
+          className={`absolute top-0 left-0 h-1 ${calendarColor} rounded-tr-lg rounded-br-lg`}
+          style={{ width: `${percentComplete}%` }}
         />
       </div>
     )
