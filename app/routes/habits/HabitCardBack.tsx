d226386bcb7e38faf650faac6fbbf9f645167546
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-03-05 18:27:23 +0100

    fix(habits): shorter words and spacings for mobile

--- app/routes/habits/HabitCardBack.tsx
+++ app/routes/habits/HabitCardBack.tsx
@@ -33,34 +33,40 @@ export function HabitCardBack({ habit }: { habit: Habit }) {
     const resetDuration = reset.endDate.diff(reset.startDate, ['days'])
     return resetDuration > longest ? resetDuration : longest
   }, Duration.fromObject({ days: 0 }))
-  const longestStreakDays = Math.floor(longestStreak.days)
+  const longestStreakDays = longestStreak.days
 
-  const currentStreak = Math.floor(
-    now.diff(habit.startDate, ['days']).toObject().days ?? 0
-  )
+  const currentStreak = now.diff(habit.startDate, ['days']).toObject().days ?? 0
   const isLongestStreak = currentStreak > longestStreakDays
 
   return (
     <>
-      <div className="grid grid-cols-5 gap-4">
+      <div className="flex gap-4">
         {Object.entries(parts)
           .filter(([_, value]) => value !== 0)
           .map(([key, value]) => (
-            <div className="text-2xl font-bold text-slate-800" key={key}>
+            <div
+              className="grid grid-cols-[auto,8px] items-baseline gap-1 text-2xl font-bold tabular-nums text-slate-800"
+              key={key}
+            >
               {Math.floor(value)}
-              <div className="text-xs font-normal">{key}</div>
+              <span className="text-xs font-normal">{key[0]}</span>
             </div>
           ))}
       </div>
-      { isLongestStreak ? (
-        <div className="text-yellow-500">
+      {isLongestStreak ? (
+        <div
+          className="pb-1 text-yellow-600/60"
+          title={`${Math.floor(currentStreak)} d`}
+        >
           <Trophy />
         </div>
       ) : null}
       {habit.resets.length > 0 && !isLongestStreak ? (
-        <div className="flex items-center gap-2 text-xs text-slate-800">
+        <div
+          className="flex items-center gap-2 pb-1 text-xs text-slate-800/30"
+          title={`${Math.floor(longestStreakDays)} d`}
+        >
           <Trophy />
-          {longestStreakDays} d
         </div>
       ) : null}
     </>
